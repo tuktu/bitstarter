9e17016e344a51813106d2955711a2507867e14b
tuktu <introverto@hotmail.com>
2013-07-16 18:27:47 -0500

    Copied web.js
    I had no choice but to copy Balaji's code since I couldn't get mine to work.

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new Buffer();
-	response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
- 	console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
